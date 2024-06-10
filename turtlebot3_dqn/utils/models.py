ef8dac1f877d644e5842c2cca0e802bcda48a390
khinggan <1516113201@qq.com>
2024-06-10 22:07:11 +0900

    Modify DQN code to make it correct

---
+++ turtlebot3_dqn/utils/models.py
@@ -0,0 +1,39 @@
+import random
+from collections import deque, namedtuple
+Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state'))
+from torch import nn
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+        self.dropout = nn.Dropout(0.2)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.relu(x)
+        x = self.layer2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.layer3(x)
+        return x
+    
