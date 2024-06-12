d2705d75290eafe3275f290cd58a14a76340c810
khinggan <1516113201@qq.com>
2024-06-12 14:07:30 +0900

    Chnage client local train

--- turtlebot3_dqn/nodes/client_local_train.py
+++ turtlebot3_dqn/nodes/client_local_train.py
@@ -11,39 +11,25 @@ Federated reinforcement learning client local training
 
 First, getting request (global model) from the FRL server, then, train it locally, finally upload the trained model to FRL server
 """
-
+import math
 import rospy
-import numpy as np
-import random
 import time
-from collections import deque, namedtuple
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from turtlebot3_dqn.srv import LocalTrain, LocalTrainResponse
-import pickle
 import importlib
-
-import torch
-from torch import nn
-import torch.optim as optim
-import torch.nn.functional as F
-
 import csv
+import pickle
+from turtlebot3_dqn.srv import LocalTrain, LocalTrainResponse
+
 
 from script.read_config import yaml_config
-
-# If you want to use CUDA. But, make sure all machines has CUDA compatibility. Otherwise, use cpu
-# device = ("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
-device = "cpu"
-print(f"Using {device} device")
-
-Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state'))
 
 CURR_CID = 1
 STAGE = 1
 LOCAL_EPISODES = 2
 ROUND = 2
+TAU = 0.005
 
 config = yaml_config()        # stages = config['FRL']['server']['stages']
 
@@ -59,132 +45,7 @@ Env = getattr(importlib.import_module(stage_module_name), 'Env')
 state_size = 26
 action_size = 5
 env = Env(action_size)
-
-class ReplayMemory(object):
-
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
-
-    def push(self, *args):
-        """Save a transition"""
-        self.memory.append(Transition(*args))
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
-
-class DQN(nn.Module):
-
-    def __init__(self, n_observations, n_actions):
-        super(DQN, self).__init__()
-        self.layer1 = nn.Linear(n_observations, 128)
-        self.layer2 = nn.Linear(128, 128)
-        self.layer3 = nn.Linear(128, n_actions)
-
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
-        x = F.relu(self.layer1(x))
-        x = F.relu(self.layer2(x))
-        return self.layer3(x)
-
-class ReinforceAgent():
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-
-        self.episode_step = 6000
-        self.target_update = 300
-        self.discount_factor = 0.99
-        self.learning_rate = 0.00025
-        self.epsilon = 1.0
-        self.epsilon_decay = 0.99
-        self.epsilon_min = 0.05
-
-        self.batch_size = 128
-        self.train_start = 128
-        self.memory = ReplayMemory(10000)
-
-        self.model = DQN(self.state_size, self.action_size).to(device)
-        self.target_model = DQN(self.state_size, self.action_size).to(device)
-        self.updateTargetModel()
-
-        self.optimizer = optim.AdamW(self.model.parameters(), lr=self.learning_rate, amsgrad=True)
-
-    def getQvalue(self, reward, next_target, done):
-        if done:
-            return reward
-        else:
-            return reward + self.discount_factor * np.amax(next_target)
-
-    def updateTargetModel(self):
-        self.target_model.load_state_dict(self.model.state_dict())
-
-    def getAction(self, state):
-        if np.random.rand() <= self.epsilon:
-            return torch.tensor([[random.randrange(self.action_size)]], device=device, dtype=torch.long)
-        else:
-            with torch.no_grad():
-                # t.max(1) will return the largest column value of each row.
-                # second column on max result is index of where max element was
-                # found, so we pick action with the larger expected reward.
-                self.q_value = self.model(state).max(1).indices.view(1, 1)
-                return self.q_value
-            # q_value = self.model(state.reshape(1, len(state)))
-            # self.q_value = q_value
-            # return np.argmax(q_value[0])
-
-    def appendMemory(self, state, action, reward, next_state):
-        self.memory.push(state, action, reward, next_state)
-
-    def trainModel(self):
-        if self.memory.__len__() < self.batch_size:
-            return
-        transitions = self.memory.sample(self.batch_size)
-        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-        # detailed explanation). This converts batch-array of Transitions
-        # to Transition of batch-arrays.
-        batch = Transition(*zip(*transitions))
-
-        # Compute a mask of non-final states and concatenate the batch elements
-        # (a final state would've been the one after which simulation ended)
-        # non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,batch.next_state)), device=device, dtype=torch.bool)
-        # non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
-
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
-        next_state_batch = torch.cat(batch.next_state)
-
-        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-        # columns of actions taken. These are the actions which would've been taken
-        # for each batch state according to policy_net
-        state_action_values = self.model(state_batch).gather(1, action_batch)
-
-        # Compute V(s_{t+1}) for all next states.
-        # Expected values of actions for non_final_next_states are computed based
-        # on the "older" target_net; selecting their best reward with max(1).values
-        # This is merged based on the mask, such that we'll have either the expected
-        # state value or 0 in case the state was final.
-        # next_state_values = torch.zeros(self.batch_size, device=device)
-        next_state_values = self.target_model(next_state_batch).max(1).values
-        # with torch.no_grad():
-        #     next_state_values[non_final_mask] = self.target_model(non_final_next_states).max(1).values
-        # Compute the expected Q values
-        expected_state_action_values = (next_state_values * self.discount_factor) + reward_batch
-
-        # Compute Huber loss
-        criterion = nn.SmoothL1Loss()
-        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
-
-        # Optimize the model
-        self.optimizer.zero_grad()
-        loss.backward()
-        # In-place gradient clipping
-        torch.nn.utils.clip_grad_value_(self.model.parameters(), 100)
-        self.optimizer.step()
+from ros1_ws.src.turtlebot3_machine_learning.turtlebot3_dqn.utils.agent import ReinforceAgent
 
 agent = ReinforceAgent(state_size, action_size)
 
@@ -208,10 +69,9 @@ def start_train(request):
     for e in range(1, LOCAL_EPISODES+1):
         done = False
         state = env.reset()
-        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-        score = 0
-        collision = 0
-        goal = 0
+        score = 0.0
+        collision_times = 0
+        goal_times = 0
         for t in range(agent.episode_step):
             action = agent.getAction(state)
 
@@ -219,13 +79,9 @@ def start_train(request):
 
             # check goal or collision
             if reward == 200:
-                goal += 1
-            
+                goal_times += 1
             if reward == -200:
-                collision += 1
-
-            reward = torch.tensor([reward], device=device)
-            next_state = torch.tensor(next_state, dtype=torch.float32, device=device).unsqueeze(0)
+                collision_times += 1
 
             agent.appendMemory(state, action, reward, next_state)
 
@@ -233,25 +89,34 @@ def start_train(request):
             score += reward
             state = next_state
 
-            if t >= 180:
+            if t >= 240:
                 rospy.loginfo("Time out!!")
                 done = True
 
+            # update epsilon
+            agent.epsilon = agent.epsilon_end + (agent.epsilon_start - agent.epsilon_end) * \
+                            math.exp(-1. * global_step / agent.epsilon_decay)
+            
+            # soft update target network
+            target_net_state_dict = agent.target_model.state_dict()
+            policy_net_state_dict = agent.model.state_dict()
+            for key in policy_net_state_dict:
+                target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+            agent.target_model.load_state_dict(target_net_state_dict)
+
             if done:
-                agent.updateTargetModel()
-                scores.append(score.item())
+                scores.append(score)
                 episodes.append(e)
                 episode_length.append(t)
-                m, s = divmod(int(time.time() - start_time), 60)
-                h, m = divmod(m, 60)
-
                 memory_lens.append(len(agent.memory))
                 epsilons.append(agent.epsilon)
+                m, s = divmod(int(time.time() - start_time), 60)
+                h, m = divmod(m, 60)
                 episode_hours.append(h)
                 episode_minutes.append(m)
                 episode_seconds.append(s)
-                collisions.append(collision)
-                goals.append(goal)
+                collisions.append(collision_times)
+                goals.append(goal_times)
 
                 rospy.loginfo('Ep: %d score: %.2f memory: %d epsilon: %.2f time: %d:%02d:%02d',
                               e, score, len(agent.memory), agent.epsilon, h, m, s)
@@ -263,11 +128,9 @@ def start_train(request):
                 break
 
             global_step += 1
-            if global_step % agent.target_update == 0:
-                rospy.loginfo("UPDATE TARGET NETWORK")
 
-        if agent.epsilon > agent.epsilon_min:
-            agent.epsilon *= agent.epsilon_decay
+        # if agent.epsilon > agent.epsilon_min:
+        #     agent.epsilon *= agent.epsilon_decay
     
     state = env.reset()
     end_time = time.time()
